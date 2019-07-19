5fa90d152349571bd266fc6329c9a98b52386f02
Bert Bruggeman <b_bruggeman@ziggo.nl>
2019-07-19 16:41:42 +0200

    Spacer

---
+++ BB74.Controls/BB/Spacer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xwt;
+using Xwt.Backends;
+using Xwt.Drawing;
+
+namespace BaseLib.Xwt.Controls
+{
+    public class Spacer : Label
+    {
+        private readonly Widget ctl;
+        private readonly Orientation direction;
+
+        public Spacer(Widget match, Orientation direction)
+        {
+            this.ctl = match;
+            this.direction = direction;
+            this.ctl.BoundsChanged += Ctl_BoundsChanged;
+            base.BackgroundColor = Colors.Transparent;
+            base.HorizontalPlacement = base.VerticalPlacement = WidgetPlacement.Fill;
+        }
+
+        private void Ctl_BoundsChanged(object sender, EventArgs e)
+        {
+            switch (this.direction)
+            {
+                case Orientation.Horizontal: this.WidthRequest = this.ctl.Size.Width; break;
+                case Orientation.Vertical: this.HeightRequest = this.ctl.Size.Height; break;
+            }
+        }
+    }
+}
