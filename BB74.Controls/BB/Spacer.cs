63e0c633f238246ae58714d95a8a46c13a7d540c
Bert Bruggeman <b_bruggeman@ziggo.nl>
2019-07-30 10:53:22 +0200

    update Spacer

--- BB74.Controls/BB/Spacer.cs
+++ BB74.Controls/BB/Spacer.cs
@@ -12,10 +12,22 @@ namespace BaseLib.Xwt.Controls
     {
         private readonly Widget ctl;
         private readonly Orientation direction;
+        private double size;
+
+        private double Width => (this.ctl?.Size.Width ?? this.size);
+        private double Height => (this.ctl?.Size.Height ?? this.size);
 
         public Spacer(Widget match, Orientation direction)
         {
             this.ctl = match;
+            this.direction = direction;
+            this.ctl.BoundsChanged += Ctl_BoundsChanged;
+            base.BackgroundColor = Colors.Transparent;
+            base.HorizontalPlacement = base.VerticalPlacement = WidgetPlacement.Fill;
+        }
+        public Spacer(double size, Orientation direction)
+        {
+            this.size = size;
             this.direction = direction;
             this.ctl.BoundsChanged += Ctl_BoundsChanged;
             base.BackgroundColor = Colors.Transparent;
@@ -26,8 +38,8 @@ namespace BaseLib.Xwt.Controls
         {
             switch (this.direction)
             {
-                case Orientation.Horizontal: this.WidthRequest = this.ctl.Size.Width; break;
-                case Orientation.Vertical: this.HeightRequest = this.ctl.Size.Height; break;
+                case Orientation.Horizontal: this.WidthRequest = this.Width; break;
+                case Orientation.Vertical: this.HeightRequest = this.Height; break;
             }
         }
     }
